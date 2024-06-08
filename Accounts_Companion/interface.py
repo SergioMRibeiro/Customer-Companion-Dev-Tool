929aa0e222ec84292d97ad533d2e959aa129a228
SergioMRibeiro <sergio.ripariu@gmail.com>
2024-06-08 17:43:02 -0300

    criação de area de informativos

--- Accounts_Companion/interface.py
+++ Accounts_Companion/interface.py
@@ -14,7 +14,7 @@ def read_json(file_path):
         return json.load(file)
 
 
-def extrair_informacoes(string):
+def who_am_i_rules(string):
     rules = r"Logged into (\S+) as (\S+) at (\S+) workspace (\S+)"
     result = re.search(rules, string)
     if result:
@@ -36,13 +36,32 @@ def test_function():
     try:
         saida1 = subprocess.check_output('vtex whoami', shell=True, stderr=subprocess.STDOUT)
         saida_decodificada1 = saida1.decode('utf-8')
-        print(extrair_informacoes(saida_decodificada1))
+        print(who_am_i_rules(saida_decodificada1))
 
         saida2 = subprocess.check_output('vtex ls', shell=True, stderr=subprocess.STDOUT)
         saida_decodificada2 = saida2.decode('utf-8')
     except subprocess.CalledProcessError as e:
         print("Ocorreu um erro:", e.output.decode('utf-8'))
-        
+
+def basic_info_pull():
+    print("infomações básicas: blá blá blá")
+
+def get_vtex_apps():
+    print("rodar vtex ls e listar apps")
+
+def verify_existent_ws():
+    print("rodar vtex worksapce list")
+
+def get_who_am_i():
+    try:
+        who_am_i_result = subprocess.check_output('vtex whoami', shell=True, stderr=subprocess.STDOUT)
+        saida_decodificada1 = who_am_i_result.decode('utf-8')
+        print(who_am_i_rules(saida_decodificada1))
+    except subprocess.CalledProcessError as e:
+        print("Ocorreu um erro:", e.output.decode('utf-8'))
+
+def activate_las_ws():
+    print("Rodar um vtex use com o ultimo ws usado")
 
 data = read_json(file_path)
 vendors = data['vendor-collection']
@@ -54,7 +73,7 @@ def run_interface():
     root.geometry("600x575")
     # !SECTION window config
     
-    # SECTION Seletor de vendor
+    # SECTION Seletor de vendor (x = 152 + 20 = 172) (y = indefinido )
     for index, vendor in enumerate(vendors):
         vendor_name = vendor['vendor-name']
         store_name = vendor['store-name']
@@ -64,16 +83,39 @@ def run_interface():
         vendor_select_button.place(x=10, y=10 + (index * 59), width=142, height=49)
     # !SECTION Seletor de vendor
 
+    # SECTION controle de informativos
+    informative_btn_frame = tk.LabelFrame(root, text="Acesso aos Informativos", padx=10, pady=10)
+    informative_btn_frame.place(x=172, y=00, width=186, height=210)
+
+    primary_info_btn = tk.Button(informative_btn_frame, text="Informações básicas", command=basic_info_pull)
+    primary_info_btn.grid(row=0, column=0, padx=0, pady=5, sticky="w")
+
+    get_vtex_apps_btn = tk.Button(informative_btn_frame, text="Apps instalados", command=get_vtex_apps)
+    get_vtex_apps_btn.grid(row=1, column=0, padx=0, pady=5, sticky="w")
+
+    verify_existent_ws_btn = tk.Button(informative_btn_frame, text="Verificar ws existente", command=basic_info_pull)
+    verify_existent_ws_btn.grid(row=2, column=0, padx=0, pady=5, sticky="w")
+
+    who_am_i_btn = tk.Button(informative_btn_frame, text="Verificar agora", command=get_who_am_i)
+    who_am_i_btn.grid(row=3, column=0, padx=0, pady=5, sticky="w")
+    
+    activate_last_ws_btn = tk.Button(informative_btn_frame, text="Ativar ultimo WS", command=get_vtex_apps)
+    activate_last_ws_btn.grid(row=4, column=0, padx=0, pady=5, sticky="w")
+    
+    # !SECTION controle de informativos
+
     # SECTION Elementos de teste
-    label = tk.Label(root, text="Bem-vindo à Ferramenta de Clientes!")
-    label.place(x=10, y=10 + (len(vendors) * 59) + 20)  # Colocar o rótulo abaixo dos botões
+    test_frame = tk.LabelFrame(root, text="Área de Teste", padx=10, pady=10)
+    test_frame.place(x=172, y=220, width=172, height=150)
 
-    quitBtn = tk.Button(root, text="Fechar", command=root.quit)
-    quitBtn.place(x=10, y=10 + (len(vendors) * 59) + 60)  # Colocar o botão "Fechar" abaixo do rótulo
+    label = tk.Label(test_frame, text="Bem-vindo à Ferramenta de Clientes!")
+    label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
-    test_command = lambda: test_function()
-    teste_btn = tk.Button(root, text="Teste algo aqui", command=test_command)
-    teste_btn.place(x=10, y=10 + (len(vendors) * 59) + 100)
+    quitBtn = tk.Button(test_frame, text="Fechar", command=root.quit)
+    quitBtn.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+
+    teste_btn = tk.Button(test_frame, text="Teste algo aqui", command=test_function)
+    teste_btn.grid(row=2, column=0, padx=5, pady=5, sticky="w")
     # !SECTION Elementos de teste
 
     root.mainloop()
--- readme.md
+++ readme.md
@@ -2,21 +2,26 @@
 
 Uma ferramenta para auxiliar no dia a dia dos devs que mexem com diversos ambientes VtexIo.
 
-# Rodando o projeto
+## Rodando o projeto
+
 Na pasta raiz execute o comando o comando abaixo apenas a primeira vez que tiver rodando o projeto para criar um virtural enviroment .
 
-    $ python -m venv venv
+`$ python -m venv venv`
 
 Os comandos a seguir são comuns para rodar o projeto. O próximo irá entrar na venv criada:
 
-    $ venv\Scripts\activate
+`$ venv\Scripts\activate`
+`(venv)$ pip install -e .`
 
-   Dessa forma irá criar o ambiente do programa e poderá executar outros comandos como para faze-lo rodar:
+Dessa forma irá criar o ambiente do programa e poderá executar outros comandos como para faze-lo rodar. (OBS.: certifique de criar o arquivo configs.json caso ele não exista.)
 
-    (venv)$ appStart
+`(venv)$ appStart`
 
-Por fim, e de modo opcional, para sair da venv pode rodar o comando: 
+Por fim, e de modo opcional, para sair da venv pode rodar o comando:
 
-    deactivate
+`deactivate`
 
+## Requisitos
 
+- python 3
+- vtex CLI
